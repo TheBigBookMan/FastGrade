f9c5cbee993c32cbce5fe1803a69c7fc3dff8d84
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-09 21:41:08 +0930

    add in templates

--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import commentRouter from './comment.js';
-// import categoryRouter from './category';
-// import userRouter from './user';
-// import adminRouter from './admin';
+// import categoryRouter from './category.js';
+// import userRouter from './user.js';
+// import adminRouter from './admin.js';
+// import feedbackRouter from './feedback.js';
+// import rubricRouter from './rubric.js';
 
 const router = Router();
 
@@ -11,5 +13,7 @@ router.use('/comment', commentRouter);
 // router.use('/category', categoryRouter);
 // router.use('/user', userRouter);
 // router.use('/admin', adminRouter);
+// router.use('/feedback', feedbackRouter);
+// router.use('/rubric', rubricRouter);
 
 export default router;
