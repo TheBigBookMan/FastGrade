214873c4cbd1b033b3d3e7685b3dcdcdee7cd235
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:38:04 +0930

    create the rubric controller template

---
+++ api/src/controllers/rubricController.js
@@ -0,0 +1,9 @@
+import logger from "../utils/logger.js";
+import rubricService from '../services/rubricService.js';
+import returnError from "../utils/returnError.js";
+
+class RubricController {
+
+}
+
+export default new RubricController();
