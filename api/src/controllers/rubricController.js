b196183a99d3f90fbe47122d4f6a00e9813ddc51
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:40:50 +0930

    create the get rubrics by user id in rubric controller

--- api/src/controllers/rubricController.js
+++ api/src/controllers/rubricController.js
@@ -3,7 +3,22 @@ import rubricService from '../services/rubricService.js';
 import returnError from "../utils/returnError.js";
 
 class RubricController {
+    async fetchRubricsByUserId (req, res) {
+        try {
 
+            const {userId} = req.params;
+
+            if(!userId) return returnError.loggerWarnUserId(res);
+
+            const rubrics = await rubricService.getRubricsByUserId(userId);
+
+            return res.json(rubrics);
+
+        } catch(err) {
+            logger.error({ msg: 'Error fetching rubrics by userId', err });
+            return returnError.internalError(res);
+        }
+    }
 }
 
 export default new RubricController();
