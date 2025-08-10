d924bc7e4a36f8570ac56bbf7f1f32e39beeedde
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:42:27 +0930

    create the create rubric router

--- api/src/routes/rubric.js
+++ api/src/routes/rubric.js
@@ -4,5 +4,6 @@ import rubricController from '../controllers/rubricController.js';
 const router = Router();
 
 router.get('/:userId', rubricController.fetchRubricsByUserId);
+router.post('/:userId', rubricController.postRubric);
 
 export default router;
