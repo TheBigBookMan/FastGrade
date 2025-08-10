a02d2fc1b74b4cde7658c1b670e0d645543580ce
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:23:34 +0930

    create the route to get category by user id in category routes

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -3,4 +3,6 @@ import categoryController from '../controllers/categoryController.js';
 
 const router = Router();
 
+router.get('/:userId', categoryController.getCategoryByUserId);
+
 export default router;
