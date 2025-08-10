9f607820fffe6d76eaf001d4e9dab22fda97ea40
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:20:14 +0930

    add in the category route to apiRoutes

--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import commentRouter from './comment.js';
-// import categoryRouter from './category.js';
+import categoryRouter from './category.js';
 // import userRouter from './user.js';
 // import adminRouter from './admin.js';
 // import feedbackRouter from './feedback.js';
@@ -10,7 +10,7 @@ import commentRouter from './comment.js';
 const router = Router();
 
 router.use('/comment', commentRouter);
-// router.use('/category', categoryRouter);
+router.use('/category', categoryRouter);
 // router.use('/user', userRouter);
 // router.use('/admin', adminRouter);
 // router.use('/feedback', feedbackRouter);
