8508f0dfdeb100c681ccb0947edbacf41f542d39
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:28:37 +0930

    create the route for creating cateogyr

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -4,5 +4,6 @@ import categoryController from '../controllers/categoryController.js';
 const router = Router();
 
 router.get('/:userId', categoryController.fetchCategoriesByUserId);
+router.post('/:userId', categoryController.postCategory);
 
 export default router;
