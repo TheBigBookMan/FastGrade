3022f6e50f733efc92ab2e07a9befca73a785d73
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:28:01 +0930

    add in the service to get categories by user id

--- api/src/services/categoryService.js
+++ api/src/services/categoryService.js
@@ -1,7 +1,15 @@
 import prisma from "../utils/prisma.js";
 
 class CategoryService {
-
+    async getCategoriesByUserId(userId) {
+        return prisma.category.findMany({
+            where: {userId},
+            orderBy: {createdAt: 'desc'},
+            include: {
+                comment: true
+            }
+        });
+    }
 }
 
 export default new CategoryService();
