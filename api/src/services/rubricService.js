45ad3b8042c24c67800d3018470b133700a31f4b
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:41:57 +0930

    create the get rubrics by user id in the rubric service

--- api/src/services/rubricService.js
+++ api/src/services/rubricService.js
@@ -1,7 +1,13 @@
 import prisma from "../utils/prisma.js";
 
 class RubricService {
-
+    async fetchRubricsByUserId (userId) {
+        return prisma.rubric.findMany({
+            where: {userId},
+            orderBy: {createdAt: 'desc'},
+            
+        });
+    }
 }
 
 export default new RubricService();
