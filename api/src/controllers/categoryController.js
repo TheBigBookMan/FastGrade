ebd2d95098246b41a18f732975cfe752db361d8e
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-10 21:22:04 +0930

    create the categoryController template

---
+++ api/src/controllers/categoryController.js
@@ -0,0 +1,9 @@
+import logger from "../utils/logger,js";
+import categoryService from '../services/categoryService.js';
+import returnError from "../utils/returnError.js";
+
+class CategoryController {
+
+}
+
+export default new CategoryController();
