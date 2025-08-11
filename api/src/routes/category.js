eaea93814c81016503fd33728c8b0be26512713b
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-11 20:19:04 +0930

    fix route

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -4,7 +4,7 @@ import categoryController from '../controllers/categoryController.js';
 const router = Router();
 
 router.get('/:userId', categoryController.fetchCategoriesByUserId);
-router.get('/category:/categoryId/user/:userId', categoryController.fetchCategoryByUserId);
+router.get('/:categoryId/user/:userId', categoryController.fetchCategoryByUserId);
 router.post('/', categoryController.postCategory);
 
 export default router;
