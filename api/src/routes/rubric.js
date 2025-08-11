29f9ad34aad9e4f8255aa98bb51c48cdb032bf57
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-11 20:22:49 +0930

    create the route to get rubric by user id in routes

--- api/src/routes/rubric.js
+++ api/src/routes/rubric.js
@@ -4,6 +4,7 @@ import rubricController from '../controllers/rubricController.js';
 const router = Router();
 
 router.get('/:userId', rubricController.fetchRubricsByUserId);
+router.get('/:rubricId/user/:userId', rubricController.fetchRubricByUserId);
 router.post('/:userId', rubricController.postRubric);
 
 export default router;
