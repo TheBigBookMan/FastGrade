cc3520afa0239f4a9273dce2b040c8ee0daf38e8
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-11 20:03:34 +0930

    setup the get category by category id and user id route

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -4,6 +4,7 @@ import categoryController from '../controllers/categoryController.js';
 const router = Router();
 
 router.get('/:userId', categoryController.fetchCategoriesByUserId);
-router.post('/:userId', categoryController.postCategory);
+router.get('/category:/categoryId/user/:userId', categoryController.fetchCategoriesByUserId);
+router.post('/', categoryController.postCategory);
 
 export default router;
