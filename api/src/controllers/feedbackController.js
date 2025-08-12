0351e1a6880b6e5802e37d2952f671b587c846b7
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-12 20:55:50 +0930

    create the fetchallFeedback controller

--- api/src/controllers/feedbackController.js
+++ api/src/controllers/feedbackController.js
@@ -3,7 +3,18 @@ import feedbackService from '../services/feedbackService.js';
 import returnError from "../utils/returnError.js";
 
 class FeedbackController {
+    async fetchAllFeedback (req, res) {
+        try {
 
+            const feedback = await feedbackService.fetchAllFeedback();
+
+            return res.json(feedback);
+
+        } catch(err) {
+            logger.error({ msg: 'Error fetching feedback' }, err);
+            return returnError.internalError(res);
+        }
+    }
 }
 
 export default new FeedbackController();
