6c4b51358f6098300e4e2df34a98a384544b8004
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-12 20:56:02 +0930

    create the fetch all feedback service

--- api/src/services/feedbackService.js
+++ api/src/services/feedbackService.js
@@ -1,7 +1,9 @@
 import prisma from "../utils/prisma";
 
 class FeedbackService {
-
+    async fetchAllFeedback() {
+        return prisma.feedback.findMany();
+    }
 }
 
 export default new FeedbackService();
