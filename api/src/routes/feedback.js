bf10d5f1341c3da799de73778398d2b4a377a7a1
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-12 21:05:41 +0930

    create the fetch all feedback by user route

--- api/src/routes/feedback.js
+++ api/src/routes/feedback.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/', feedbackController.fetchAllFeedback);
 router.post('/', feedbackController.createFeedback);
 router.get('/:feedbackId/user/:userId', feedbackController.fetchFeedbackByUser);
+router.get('/user/:userId', feedbackController.fetchAllFeedbackByUser);
 
 export default router;
