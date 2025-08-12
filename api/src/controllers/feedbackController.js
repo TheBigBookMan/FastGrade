c31ddadd66cb0acfac37c9470a1912bf3351f1e6
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-12 20:59:42 +0930

    create the create feedback controller

--- api/src/controllers/feedbackController.js
+++ api/src/controllers/feedbackController.js
@@ -15,6 +15,25 @@ class FeedbackController {
             return returnError.internalError(res);
         }
     }
+
+    async createFeedback(req, res) {
+        try {
+
+            const {title, comment, userId} = req.body;
+
+            const newFeedback = await feedbackService.createFeedback({
+                title,
+                comment,
+                userId
+            });
+
+            return res.status(201).json(newFeedback);
+
+        } catch(err) {
+            logger.error({ msg: 'Error creating feedback', err });
+            return returnError.internalError(res);
+        }
+    }
 }
 
 export default new FeedbackController();
