59c2484178b1d240979dc79cda21b25ebf100e11
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-12 20:59:35 +0930

    create the create feedback route

--- api/src/routes/feedback.js
+++ api/src/routes/feedback.js
@@ -4,5 +4,6 @@ import feedbackController from '../controllers/feedbackController.js';
 const router = Router();
 
 router.get('/', feedbackController.fetchAllFeedback);
+router.post('/', feedbackController.createFeedback);
 
 export default router;
