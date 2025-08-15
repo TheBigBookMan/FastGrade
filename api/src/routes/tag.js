7459f745352e507901a5269312d2a0482e137467
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-15 12:38:39 +0930

    create the create tag route

--- api/src/routes/tag.js
+++ api/src/routes/tag.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/:userId', tagController.fetchTagsByUserId);
 router.get('/:tagId/user/:userId', tagController.fetchTagByUserId);
+router.post('/', tagController.createTag);
 
 export default router;
