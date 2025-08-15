3f5c5e755661092763150a3fa2e7c0d9434233dd
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-15 12:31:02 +0930

    add in the service to fetch all tags by user id

--- api/src/services/tagService.js
+++ api/src/services/tagService.js
@@ -1,7 +1,11 @@
 import prisma from "../utils/prisma.js";
 
 class TagService {
-
+    async fetchAllTagsByUserId (userId) {
+        return prisma.tag.findMany({
+            where: {userId}
+        });
+    } 
 }
 
 export default new TagService();
