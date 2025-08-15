d387a803b0eea4ddfb9b7b40d6d470fb20641651
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-15 12:33:52 +0930

    create the route for fetching tag by user id

--- api/src/routes/tag.js
+++ api/src/routes/tag.js
@@ -4,5 +4,6 @@ import tagController from '../controllers/tagController.js';
 const router = Router();
 
 router.get('/:userId', tagController.fetchTagsByUserId);
+router.get('/:tagId/user/:userId', tagController.fetchTagByUserId);
 
 export default router;
