10107e2a91356de2a47d34faa4e7767101d15475
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-15 12:30:55 +0930

    add in the controller to fetch all tags by user id

--- api/src/controllers/tagController.js
+++ api/src/controllers/tagController.js
@@ -3,7 +3,21 @@ import tagService from '../services/tagService.js';
 import returnError from "../utils/returnError.js";
 
 class TagController {
+    async fetchTagsByUserId(req, res) {
+        try {
 
+            const {userId} = req.params;
+            if(!userId) return returnError.loggerWarnUserId(res);
+
+            const tags = await tagService.fetchAllTagsByUserId(userId);
+
+            return res.json(tags);
+
+        } catch(err) {
+            logger.error({ msg: 'Error fetching tags by userId', err });
+            return returnError.internalError(res);
+        }
+    }
 }
 
 export default new TagController();
