0a6becbab96354ab7e126c5bd7a44114efc032d2
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-16 21:23:41 +0930

    add in routes and components to render

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,6 +3,10 @@ import LoginPage from './pages/auth/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import { ProtectedRoute } from './components/common/auth/ProtectedRoute';
 import { AuthProvider } from './contexts/AuthContext';
+import CommentsPage from './pages/CommentsPage';
+import CategoriesPage from './pages/CategoriesPage';
+import RubricsPage from './pages/RubricsPage';
+import FeedbackPage from './pages/FeedbackPage';
 
 function App() {
     return (
@@ -12,6 +16,10 @@ function App() {
 					<Route path='/login' element={<LoginPage />} />
 
 					<Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+					<Route path="/comments" element={<ProtectedRoute><CommentsPage /></ProtectedRoute>} />
+					<Route path="/categories" element={<ProtectedRoute><CategoriesPage /></ProtectedRoute>} />
+					<Route path="/rubrics" element={<ProtectedRoute><RubricsPage /></ProtectedRoute>} />
+					<Route path="/feedback" element={<ProtectedRoute><FeedbackPage /></ProtectedRoute>} />
 
 					<Route path='/' element={<Navigate to='/dashboard' replace />} />
 				</Routes>
