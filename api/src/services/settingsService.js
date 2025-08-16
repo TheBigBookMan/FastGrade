1317f992e6f3fb41e3c62e9abbd824d44de07b53
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-16 15:07:09 +0930

    add in the fetch settings by user id service

--- api/src/services/settingsService.js
+++ api/src/services/settingsService.js
@@ -1,6 +1,11 @@
 import prisma from '../utils/prisma.js';
 
 class SettingsService {
+    async getSettingsByUserId(userId) {
+        return prisma.settings.findUnique({
+            where: { userId }
+        });
+    }
 }
 
 export default new SettingsService();
