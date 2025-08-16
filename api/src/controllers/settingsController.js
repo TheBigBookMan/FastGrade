f9de87d4800fd46438f42c57f66b6cea763dbd41
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-16 15:07:00 +0930

    add in the fetch settings by user Id controller

--- api/src/controllers/settingsController.js
+++ api/src/controllers/settingsController.js
@@ -3,6 +3,20 @@ import settingsService from '../services/settingsService.js';
 import returnError from '../utils/returnError.js';
 
 class SettingsController {
+    async fetchSettingsByUserId(req, res) {
+        try {
+            const { userId } = req.params;
+
+            if(!userId) return returnError.loggerWarnUserId(res);
+
+            const settings = await settingsService.getSettingsByUserId(userId);
+
+            return res.json(settings);
+        } catch (err) {
+            logger.error({ msg: 'Error fetching settings by user id', err });
+            return returnError.internalError(res);
+        }
+    }
 }
 
 export default new SettingsController();
