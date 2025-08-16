e9b8bd01b5184524610dcc74d3591878cb90ebeb
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-16 20:43:26 +0930

    wrap app.tsx in auth provider

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,18 +2,21 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/auth/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import { ProtectedRoute } from './components/common/auth/ProtectedRoute';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
     return (
-        <div className="min-h-screen bg-gray-100">
-			<Routes>
-				<Route path='/login' element={<LoginPage />} />
+		<AuthProvider>
+			<div className="min-h-screen bg-gray-100">
+				<Routes>
+					<Route path='/login' element={<LoginPage />} />
 
-				<Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+					<Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
 
-				<Route path='/' element={<Navigate to='/dashboard' replace />} />
-			</Routes>
-        </div>
+					<Route path='/' element={<Navigate to='/dashboard' replace />} />
+				</Routes>
+			</div>
+		</AuthProvider>
     )
 }
 
