78f26fdf6b1d9a2b143b5061ef5b8133a8aca6e5
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-16 14:36:37 +0930

    set up settings routes template

---
+++ api/src/routes/settings.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import settingsController from '../controllers/settingsController.js';
+
+const router = Router();
+
+// router.get('/:userId', settingsController.fetchSettingsByUserId);
+// router.post('/', settingsController.postSettings);
+
+export default router;
