7aa4cc205732ee7796e73895fa1dbce091dc79d5
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-16 14:36:45 +0930

    setup settings controller template

---
+++ api/src/controllers/settingsController.js
@@ -0,0 +1,8 @@
+import logger from '../utils/logger.js';
+import settingsService from '../services/settingsService.js';
+import returnError from '../utils/returnError.js';
+
+class SettingsController {
+}
+
+export default new SettingsController();
