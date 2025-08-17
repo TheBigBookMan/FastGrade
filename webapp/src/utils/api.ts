f4a5f185e9630d91d1476936f52e2e763e943cc6
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-17 14:11:17 +0930

    update proper types for environment variables

--- webapp/src/utils/api.ts
+++ webapp/src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 
+console.log('import.meta:', import.meta);
+console.log('import.meta.env:', import.meta.env);
 export const api: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
