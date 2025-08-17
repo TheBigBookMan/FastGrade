a6a1228889299ffb8a3a3ffbb0a2246d80e7f952
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-17 14:14:42 +0930

    fix base url

--- webapp/src/utils/api.ts
+++ webapp/src/utils/api.ts
@@ -1,7 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
-console.log('import.meta:', import.meta);
-console.log('import.meta.env:', import.meta.env);
 export const api: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/',
     timeout: 10000,
