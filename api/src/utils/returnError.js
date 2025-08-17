04a0094576badcc73a23ac7f6bfc278060635edf
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-17 13:34:21 +0930

    update the response error messages

--- api/src/utils/returnError.js
+++ api/src/utils/returnError.js
@@ -3,16 +3,28 @@ import logger from "./logger.js";
 class ReturnError {
     loggerWarnUserId = (res) => {
         logger.warn('Missing userId in request params');
-        return res.status(400).json({ error: 'User ID is required' });
+        return res.status(400).json({ 
+            success: false,
+            message: 'User ID is required',
+            timestamp: new Date().toISOString()
+        });
     };
 
     loggerWarnRequiredAttribute = (res, entity, attribute) => {
         logger.warn(`Missing ${entity} ${attribute} in request`);
-        return res.status(400).json({ error: `${entity} ${attribute} is required` });
+        return res.status(400).json({ 
+            success: false,
+            message: `${entity} ${attribute} is required` ,
+            timestamp: new Date().toISOString()
+        });
     };
 
     internalError = (res) => {
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({             
+            success: false,
+            message: 'Internal server error',
+            timestamp: new Date().toISOString() 
+        });
     };
 }
 
