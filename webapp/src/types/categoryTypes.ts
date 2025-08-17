15b3e4e161471743eca5b340554b031eeccb35d7
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-17 19:03:38 +0930

    create the interface for CategoryResponse

--- webapp/src/types/categoryTypes.ts
+++ webapp/src/types/categoryTypes.ts
@@ -1,8 +1,16 @@
 export interface Category {
     id: string;
     name: string;
+    userId: string;
     description: string;
     order: number;
     createdAt: string;
     updatedAt: string;
 }
+
+export interface CategoryResponse {
+    success: boolean;
+    message?: string;
+    data?: Category | Category[]
+}
+
