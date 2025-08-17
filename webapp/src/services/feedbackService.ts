a73ce53099f739a18e060f1f6500f9a4a40f3885
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-17 13:01:21 +0930

    update creating feedback submission in the feedback service

--- webapp/src/services/feedbackService.ts
+++ webapp/src/services/feedbackService.ts
@@ -1,9 +1,10 @@
 import { api } from "../utils/api";
-import {FeedbackData, Feedback} from "../types/feedbackTypes";
+import {FeedbackData, FeedbackResponse} from "../types/feedbackTypes";
 
 class FeedbackService {
-    async createFeedback(data: FeedbackData): Promise<Feedback> {
-        return await api.post('/feedback', data);
+    async createFeedback(data: FeedbackData): Promise<FeedbackResponse> {
+        const response = await api.post('/feedback', data);
+        return response.data;
     }
 }
 
