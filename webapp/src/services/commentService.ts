2d5b9d3fc70c44ff5e41dbbe5014eaa26a724a3d
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-22 20:48:01 +0930

    create the fetch comment service

--- webapp/src/services/commentService.ts
+++ webapp/src/services/commentService.ts
@@ -6,6 +6,11 @@ class CommentService {
         const response = await api.post('/comment', data);
         return response.data;
     }
+
+    async getComments(userId: string, includeCategories = false): Promise<CommentResponse> {
+        const response = await api.get(`/comment/${userId}?includeCategories=${includeCategories}`);
+        return response.data;
+    }
 }
 
 export default new CommentService();
