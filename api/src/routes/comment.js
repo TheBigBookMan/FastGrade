a30cc6578f3b8e8f639286c2c53d62a7a424656b
Benjamin Smerd <bjsmerd@gmail.com>
2025-09-13 15:20:05 +0930

    add in the update favourite comment route

--- api/src/routes/comment.js
+++ api/src/routes/comment.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get('/:userId', commentController.fetchCommentsByUserId);
 router.get('/:commentId/user/:userId', commentController.fetchCommentByUserId);
 router.put('/:commentId/user/:userId', commentController.updateComment);
+router.put('/:commentId/user/:userId/favourite', commentController.updateCommentFavourite);
 router.delete('/:commentId/user/:userId', commentController.deleteComment);
 router.post('/', commentController.postComment);
 
