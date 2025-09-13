5739081ec66ee01d4c1a32d6d56c27682d76d707
Benjamin Smerd <bjsmerd@gmail.com>
2025-09-13 12:26:00 +0930

    create the delete comment route

--- api/src/routes/comment.js
+++ api/src/routes/comment.js
@@ -5,7 +5,8 @@ const router = Router();
 
 router.get('/:userId', commentController.fetchCommentsByUserId);
 router.get('/:commentId/user/:userId', commentController.fetchCommentByUserId);
-router.put('/:commentId/user/:userId', commentController.updateCommentByUserId);
+router.put('/:commentId/user/:userId', commentController.updateComment);
+router.delete('/:commentId/user/:userId', commentController.deleteComment);
 router.post('/', commentController.postComment);
 
 export default router;
